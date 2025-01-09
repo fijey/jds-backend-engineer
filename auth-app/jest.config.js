1b171072d416bca3b656532bffdb91942973eff1
Fajar Mukti <fajarmukti9@gmail.com>
2025-01-09 09:54:14 +0700

    feat(auth-app): init jest unit test

---
+++ auth-app/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+    "@models/(.*)": "<rootDir>/src/models/$1",
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
+    "@controllers/(.*)": "<rootDir>/src/controllers/$1",
+    "@routes/(.*)": "<rootDir>/src/routes/$1"
+  },
+  setupFiles: ['dotenv/config']
+};
